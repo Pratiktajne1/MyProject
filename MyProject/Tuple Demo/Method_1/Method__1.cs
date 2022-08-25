d80c00516914e57d97953d213bdef168bed32871
Pratiktajne1 <pratiktajne1@gmail.com>
2022-08-25 11:50:19 +0530

    25/08/2022

---
+++ MyProject/Tuple Demo/Method_1/Method__1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyProject.Tuple_Demo.Method_1
+{
+    class Method__1
+    {
+        public Tuple<int,int> Display(int a, int b)
+        {
+            int sum = a + b;
+            int pro = a * b;
+            return Tuple.Create<int,int>(sum,pro);
+        }
+    }
+}
---
+++ MyProject/Tuple Demo/Method_1/Method__1_Main.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyProject.Tuple_Demo.Method_1
+{
+    class Method__1_Main
+    {
+        static void Main(string[] args)
+        {
+            Method__1 M_1 = new Method__1();
+            Tuple<int ,int> t = M_1.Display(12, 34);
+
+            Console.WriteLine(t.Item1);
+            Console.WriteLine(t.Item2);
+        }
+    }
+}
